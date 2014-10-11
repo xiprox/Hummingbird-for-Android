2656a32d7f87eeba90b6ddc68e3fbcdc51fdd76f
Mihca Ceylan <bc.mihca@gmail.com>
2014-10-11 20:44:58 +0300

    Catch "hummingbird.me" Links

---
+++ app/src/main/java/tr/bcxip/hummingbird/CatchLink.java
@@ -0,0 +1,37 @@
+package tr.bcxip.hummingbird;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * Created by mhca on 10/11/2014.
+ */
+public class CatchLink extends Activity {
+    String mLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mLink = getIntent().getDataString();
+        if(mLink.contains("/anime/")){
+            mLink = mLink.replace("http://hummingbird.me/anime/", "");
+            String sCheck = mLink.substring(mLink.length() -1, mLink.length());
+            //check if url ends with /fate-zero'/'
+            if (sCheck.contains("/")){
+                mLink = mLink.substring(0, mLink.length() - 1);
+            }
+            Intent intent = new Intent(CatchLink.this, AnimeDetailsActivity.class);
+            //intent.putExtra(AnimeDetailsActivity.ARG_ID, Integer.parseInt(mLink));
+            intent.putExtra(AnimeDetailsActivity.ARG_ID, mLink);
+            startActivity(intent);
+            Toast.makeText(getApplicationContext(), mLink, Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(getApplicationContext(), mLink, Toast.LENGTH_SHORT).show();
+        }
+
+
+
+    }
+}
