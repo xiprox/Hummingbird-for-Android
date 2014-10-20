020eb37a7213f424ba294322ed9d56de5bd44a49
İhsan Işık <x.ihsan@gmail.com>
2014-10-20 16:05:57 +0200

    Implement swipe refresh iin feed

--- app/src/main/java/tr/bcxip/hummingbird/FeedFragment.java
+++ app/src/main/java/tr/bcxip/hummingbird/FeedFragment.java
@@ -1,9 +1,11 @@
 package tr.bcxip.hummingbird;
 
-import android.support.v4.app.Fragment;
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,7 +25,7 @@ import tr.bcxip.hummingbird.managers.PrefManager;
 /**
  * Created by Hikari on 10/11/14.
  */
-public class FeedFragment extends Fragment {
+public class FeedFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     public static final String ARG_USERNAME = "username";
 
@@ -35,6 +37,7 @@ public class FeedFragment extends Fragment {
 
     ListView mList;
     ViewFlipper mFlipper;
+    SwipeRefreshLayout mSwipeRefresh;
 
     List<Story> mItems;
 
@@ -57,6 +60,13 @@ public class FeedFragment extends Fragment {
         mList = (ListView) rootView.findViewById(R.id.feed_list);
         mFlipper = (ViewFlipper) rootView.findViewById(R.id.feed_view_flipper);
 
+        mSwipeRefresh = (SwipeRefreshLayout) rootView.findViewById(R.id.feed_swipe_refresh);
+        mSwipeRefresh.setOnRefreshListener(this);
+
+        Resources res = getResources();
+        mSwipeRefresh.setColorSchemeColors(res.getColor(R.color.apptheme_primary),
+                res.getColor(R.color.apptheme_primary_dark));
+
         /**
          * Check for any username arguments being passed to the fragment. If one is passed, we will
          * load the feed for that username. If no argument is passed, we load the feed for the
@@ -78,9 +88,27 @@ public class FeedFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         if (loadTask != null) loadTask.cancel(true);
+        if (mSwipeRefresh != null && mSwipeRefresh.isRefreshing())
+            mSwipeRefresh.setRefreshing(false);
     }
 
-    protected class LoadTask extends AsyncTask<Void, Void, String> {
+    @Override
+    public void onRefresh() {
+        if (loadTask != null && !loadTask.isCancelled())
+            loadTask.cancel(false);
+
+        loadTask = new LoadTask();
+        loadTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
+    private class LoadTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            if (mSwipeRefresh != null) mSwipeRefresh.setRefreshing(true);
+            if (mFlipper.getDisplayedChild() == 1) mFlipper.showPrevious();
+        }
 
         @Override
         protected String doInBackground(Void... voids) {
@@ -97,10 +125,11 @@ public class FeedFragment extends Fragment {
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
 
-            if (result.equals(Results.RESULT_SUCCESS)) {
+            if (result.equals(Results.RESULT_SUCCESS))
                 mList.setAdapter(new FeedAdapter(context, R.layout.item_story_comment, mItems, username));
-                if (mFlipper.getDisplayedChild() == 0) mFlipper.showNext();
-            }
+
+            if (mFlipper.getDisplayedChild() == 0) mFlipper.showNext();
+            if (mSwipeRefresh != null) mSwipeRefresh.setRefreshing(false);
         }
     }
 }
