2c4f1316d3b18c3714a49a4be109f430c5e1defe
Ihsan Isik <x.ihsan@gmail.com>
2014-11-14 15:41:20 +0100

    Keeping up with API changes

--- app/src/main/java/tr/bcxip/hummingbird/api/objects/AnimeV2.java
+++ app/src/main/java/tr/bcxip/hummingbird/api/objects/AnimeV2.java
@@ -8,99 +8,95 @@ import tr.bcxip.hummingbird.utils.Utils;
  * Created by Hikari on 10/8/14.
  */
 public class AnimeV2 {
-    Data anime;
+    String id;
+    Titles titles;
+    String slug;
+    String synopsis;
+    String started_airing_date;
+    String finished_airing_date;
+    String youtube_trailer_id;
+    String age_rating;
+    int episode_count;
+    int episode_length;
+    String poster_image;
+    String show_type;
+    double community_rating;
+    List<String> genres;
+    List<GalleryImage> gallery_images;
 
-    private class Data {
-        String id;
-        Titles titles;
-        String slug;
-        String synopsis;
-        String started_airing_date;
-        String finished_airing_date;
-        String youtube_trailer_id;
-        String age_rating;
-        int episode_count;
-        int episode_length;
-        String poster_image;
-        String show_type;
-        double community_rating;
-        List<String> genres;
-        List<String> screencaps;
-
-        private class Titles {
-            String canonical;
-            String english;
-            String romaji;
-        }
+    private class Titles {
+        String canonical;
+        String english;
+        String romaji;
     }
 
     public String getId() {
-        return anime.id;
+        return id;
     }
 
     public String getSlug() {
-        return anime.slug;
+        return slug;
     }
 
     public String getCanonicalTitle() {
-        return anime.titles.canonical;
+        return titles.canonical;
     }
 
     public String getEnglishTitle() {
-        return anime.titles.english;
+        return titles.english;
     }
 
     public String getRomajiTitle() {
-        return anime.titles.romaji;
+        return titles.romaji;
     }
 
     public String getSynopsis() {
-        return anime.synopsis;
+        return synopsis;
     }
 
     public String getCoverImageLink() {
-        return anime.poster_image;
+        return poster_image;
     }
 
     public List<String> getGenres() {
-        return anime.genres;
+        return genres;
     }
 
     public String getType() {
-        return anime.show_type;
+        return show_type;
     }
 
     public long getAiringStartDate() {
-        return anime.started_airing_date != null ?
-                Utils.getTimestampFromAirDate(anime.started_airing_date) : 0;
+        return started_airing_date != null ?
+                Utils.getTimestampFromAirDate(started_airing_date) : 0;
     }
 
     public long getAiringFinishedDate() {
-        return anime.finished_airing_date != null ?
-                Utils.getTimestampFromAirDate(anime.finished_airing_date) : 0;
+        return finished_airing_date != null ?
+                Utils.getTimestampFromAirDate(finished_airing_date) : 0;
     }
 
-    public List<String> getSreencaps() {
-        return anime.screencaps;
+    public List<GalleryImage> getGalleryImages() {
+        return gallery_images;
     }
 
     public String getTrailer() {
-        return anime.youtube_trailer_id;
+        return youtube_trailer_id;
     }
 
     public double getCommunityRating() {
-        return anime.community_rating;
+        return community_rating;
     }
 
     public String getAgeRating() {
-        return anime.age_rating;
+        return age_rating;
     }
 
     public int getEpisodeCount() {
-        return anime.episode_count;
+        return episode_count;
     }
 
     public int getEpisodeLength() {
-        return anime.episode_length;
+        return episode_length;
     }
 }
---
+++ app/src/main/java/tr/bcxip/hummingbird/api/objects/GalleryImage.java
@@ -0,0 +1,22 @@
+package tr.bcxip.hummingbird.api.objects;
+
+/**
+ * Created by ix on 11/14/14.
+ */
+public class GalleryImage {
+    String id;
+    String thumb;
+    String original;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getThumb() {
+        return thumb;
+    }
+
+    public String getOriginal() {
+        return original;
+    }
+}
